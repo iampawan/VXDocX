46b7a11b24bdfee9ab83bc69dac583b68cce5753
Pawan Kumar <mtechviral@gmail.com>
2020-08-13 22:34:11 +0530

    Added some extensions

--- sidebars.js
+++ sidebars.js
@@ -1,9 +1,10 @@
 module.exports = {
   someSidebar: {
-    "Getting Started": ['install', 'release-notes'],
-    "Customization": ['colors'],
-    "Layout": ['padding', 'text', 'flex', 'box'],
-    "Typography": ['text'],
-    Features: ['responsive'],
+    "Getting Started": ["install", "release-notes"],
+    Customization: ["colors"],
+    Layout: ["padding", "text", "flex", "box"],
+    Typography: ["text"],
+    Features: ["responsive"],
+    Extensions: ["context", "string"],
   },
 };
